98eed59c87e92e1e1403169c09838513969fbefc
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-08-24 09:13:06 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -62,17 +62,6 @@ public class User {
     }
 
     @Override
-    public String toString() {
-        return
-                "User{" +
-                        "id=" + id +
-                        ", name='" + name + '\'' +
-                        ", lastName='" + lastName + '\'' +
-                        ", age=" + age +
-                        '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -83,6 +72,17 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return
+                "User{" +
+                        "id=" + id +
+                        ", name='" + name + '\'' +
+                        ", lastName='" + lastName + '\'' +
+                        ", age=" + age +
+                        '}';
     }
 
 }
